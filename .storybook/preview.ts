050024891176e63de351025a58f9c037a8051c6d
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-10-05 22:36:24 +0200

    init

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from "@storybook/svelte";
+import "../static/app.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
